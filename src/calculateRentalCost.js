6e042249b875efb3a0288ed035060482d2af7e6b
VM93 <vovamuzyka93@gmail.com>
2025-07-07 15:16:43 +0300

    Commit message

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,13 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const rent = 40;
+  let amount = rent * days;
+
+  days >= 3 && days <= 6 && (amount -= 20);
+  days > 6 && (amount -= 50);
+
+  return amount;
 }
 
 module.exports = calculateRentalCost;
